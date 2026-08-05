8ec5ac630b42b969ccf7dbc5604f65791a44dbeb
Salman Afridi <salmanafridi.dev@gmail.com>
2026-08-05 13:58:29 +0500

    Refactor user controller to use asyncHandler for improved error handling and code readability

--- 03Notes-api/src/controllers/user.controller.js
+++ 03Notes-api/src/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 import {
   registerService,
@@ -8,74 +9,54 @@ import {
   refreshTokenService,
 } from "../services/auth.service.js";
 
-const registerUser = async (req, res, next) => {
-  try {
-    const data = req.body;
-    const user = await registerService(data);
-    return res
-      .status(201)
-      .json(new ApiResponse(201, user, "User registered successfully"));
-  } catch (error) {
-    next(error);
+const registerUser = asyncHandler(async (req, res, next) => {
+  const data = req.body;
+  const user = await registerService(data);
+  return res
+    .status(201)
+    .json(new ApiResponse(201, user, "User registered successfully"));
+});
+
+const loginUser = asyncHandler(async (req, res, next) => {
+  const loginData = req.body;
+
+  const loggedInUser = await loginService(loginData);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, loggedInUser, "User logged in successfully"));
+});
+
+const refreshAccessToken = asyncHandler(async (req, res, next) => {
+  const { refreshToken } = req.body;
+
+  if (!refreshToken) {
+    throw new ApiError(404, "Refresh Token is required");
   }
-};
 
-const loginUser = async (req, res, next) => {
-  try {
-    const loginData = req.body;
+  const accessToken = await refreshTokenService(refreshToken);
 
-    const loggedInUser = await loginService(loginData);
-
-    return res
-      .status(200)
-      .json(new ApiResponse(200, loggedInUser, "User logged in successfully"));
-  } catch (error) {
-    next(error);
-  }
-};
-
-const refreshAccessToken = async (req, res, next) => {
-  try {
-    const { refreshToken } = req.body;
-
-    if (!refreshToken) {
-      throw new ApiError(404, "Refresh Token is required");
-    }
-
-    const accessToken = await refreshTokenService(refreshToken);
-
-    return res
-      .status(200)
-      .json(
-        new ApiResponse(
-          200,
-          accessToken,
-          "Access token refreshed successfully",
-        ),
-      );
-  } catch (error) {
-    next(error);
-  }
-};
-
-const logoutUser = async (req, res, next) => {
-  try {
-    const userId = req.user?._id;
-
-    const loggedOutUser = await logoutService(userId);
-
-    return res.status(200).json(
-      new ApiResponse(
-        200,
-        {
-          loggedOutUser,
-        },
-        "User logged out successfully",
-      ),
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, accessToken, "Access token refreshed successfully"),
     );
-  } catch (error) {
-    next(error);
-  }
-};
+});
+
+const logoutUser = asyncHandler(async (req, res, next) => {
+  const userId = req.user?._id;
+
+  const loggedOutUser = await logoutService(userId);
+
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        loggedOutUser,
+      },
+      "User logged out successfully",
+    ),
+  );
+});
 
 export { registerUser, loginUser, logoutUser, refreshAccessToken };
