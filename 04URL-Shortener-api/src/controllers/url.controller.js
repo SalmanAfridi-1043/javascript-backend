27c4b9e604403580985ff4c9f6d18e0b1f7b3f26
Salman Afridi <salmanafridi.dev@gmail.com>
2026-08-07 11:27:03 +0500

    createUrl controller done

--- 04URL-Shortener-api/src/controllers/url.controller.js
+++ 04URL-Shortener-api/src/controllers/url.controller.js
@@ -3,16 +3,14 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { createUrlService } from "../services/url.service.js";
 
 const createUrl = asyncHandler(async (req, res, next) => {
-  const { originalUrl } = req.body;
+  const { originalUrl, expiresAt } = req.body;
   const userId = req.user?._id;
 
-  const createdUrl = await createUrlService(originalUrl, userId);
+  const createdUrl = await createUrlService(originalUrl, expiresAt, userId);
 
   return res
     .status(200)
-    .json(
-      new ApiResponse(200, createdUrl, "New short URL created successfully"),
-    );
+    .json(new ApiResponse(200, createdUrl, "URL code created successfully"));
 });
 
 export { createUrl };
