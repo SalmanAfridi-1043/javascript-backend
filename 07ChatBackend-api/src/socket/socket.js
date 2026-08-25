a907ab95f26249146755aa1fcadf32e111378178
Salman Afridi <salmanafridi.dev@gmail.com>
2026-08-25 17:13:50 +0500

    join socket rooms added

--- 07ChatBackend-api/src/socket/socket.js
+++ 07ChatBackend-api/src/socket/socket.js
@@ -1,5 +1,7 @@
 import { Server } from "socket.io";
 import { socketAuthMiddleware } from "../middleware/socketAuth.middleware.js";
+import { Conversation } from "../models/conversation.model.js";
+import { ApiError } from "../utils/ApiError.js";
 
 export const initializeSocket = (server) => {
   const io = new Server(server, {
@@ -8,26 +10,61 @@ export const initializeSocket = (server) => {
     },
   });
 
-  //   What does io.use() mean? It's Socket.IO's middleware registration.It means:("Before allowing a socket connection to reach connection, run this middleware.");
+  // Socket authentication middleware
   io.use(socketAuthMiddleware);
 
-  //   means: "Whenever a client successfully connects to our Socket.IO server, run this function."
+  // Runs whenever a client successfully connects
   io.on("connection", (socket) => {
     console.log(`Socket connected: ${socket.id}`);
 
-    //   It fires when the connection is lost
+    // Join a conversation room
+    socket.on("conversation:join", async (conversationId) => {
+      try {
+        // Find conversation
+        const conversation = await Conversation.findById(conversationId);
+
+        if (!conversation) {
+          throw new ApiError(404, "Conversation not found");
+        }
+
+        // Check whether authenticated user is a participant
+        const isParticipant = conversation.participants.some(
+          (participant) =>
+            participant.toString() === socket.user._id.toString(),
+        );
+
+        if (!isParticipant) {
+          throw new ApiError(
+            403,
+            "You are not authorized to join this conversation",
+          );
+        }
+
+        // Join conversation room
+        const roomName = `conversation:${conversationId}`;
+
+        await socket.join(roomName);
+
+        console.log(`${socket.user.username} joined ${roomName}`);
+      } catch (error) {
+        console.error(`Failed to join conversation: ${error.message}`);
+
+        // For Socket.IO events, we don't have Express's (res.status(...).json(...))
+        // Instead, we can send an event back to the client:
+
+        // Send error back to this socket
+        socket.emit("conversation:error", {
+          statusCode: error.statusCode || 500,
+          message: error.message || "Failed to join conversation",
+        });
+      }
+    });
+
+    // Fires when this socket disconnects
     socket.on("disconnect", () => {
       console.log(`Socket disconnected: ${socket.id}`);
     });
-  });   
+  });
 
   return io;
 };
-
-//new Server(server) - creates the Socket.IO server and attaches it to our existing HTTP server.
-
-// HTTP Server
-//      │
-//      ├── Express → REST API
-//      │
-//      └── Socket.IO → Real-time communication
